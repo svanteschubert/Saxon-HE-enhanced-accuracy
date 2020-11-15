be6e5d57b9298f7c806f46151035a943d49b24ef
Svante Schubert <svante.schubert@gmail.com>
2020-11-16 00:16:07 +0100

    Add rounding examples with postive and negative scale to test

--- src/test/java/net/sf/saxon/DecimalBasedFloatingPointTest.java
+++ src/test/java/net/sf/saxon/DecimalBasedFloatingPointTest.java
@@ -1,5 +1,7 @@
 package net.sf.saxon;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.logging.Logger;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +13,11 @@ public class DecimalBasedFloatingPointTest {
     void testPrecision() {
         try{
             Transform.main(MAIN_ARGS);
+            System.out.println("\n");
+            System.out.println("Rounding half-even 123456789.987654321 with scale  2 to " + new BigDecimal("123456789.987654321").divide(BigDecimal.ONE, 2, RoundingMode.HALF_EVEN));
+            System.out.println("Rounding half-even 123456789.987654321 with scale -2 to " + new BigDecimal("123456789.987654321").divide(BigDecimal.ONE, -2, RoundingMode.HALF_EVEN));
+            System.out.println("Rounding half-even 123456789.987654321 with scale -2 to " + new BigDecimal("123456789.987654321").divide(BigDecimal.ONE, -2, RoundingMode.HALF_EVEN).toPlainString());
+            System.out.println("Rounding half-even 123456789 with scale -2 to " + new BigDecimal("123456789").divide(BigDecimal.ONE, -2, RoundingMode.HALF_EVEN).toPlainString());
         }catch(Throwable t){
             Logger.getLogger(getClass().getName()).severe(t.getLocalizedMessage());
             throw t;            
